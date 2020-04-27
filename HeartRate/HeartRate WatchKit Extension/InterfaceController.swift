490d3fb66bc15ad5b501eb4f2730d7c856c6372f
Kenshoshibaura <50857122+Kenshoshibaura@users.noreply.github.com>
2020-04-27 22:57:53 +0900

    雛形

---
+++ HeartRate/HeartRate WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  HeartRate WatchKit Extension
+//
+//  Created by 西澤健将 on 2020/04/27.
+//  Copyright © 2020 西澤健将. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ HeartRate/HeartRateTests/HeartRateTests.swift
@@ -0,0 +1,34 @@
+//
+//  HeartRateTests.swift
+//  HeartRateTests
+//
+//  Created by 西澤健将 on 2020/04/27.
+//  Copyright © 2020 西澤健将. All rights reserved.
+//
+
+import XCTest
+@testable import HeartRate
+
+class HeartRateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
